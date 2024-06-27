2fa568c86581529ff78a7204e7aae97093c53e24
Marva9906 <omaralbertovalderramahome@gmail.com>
2024-06-27 14:55:35 -0500

    se agrega el proyecto

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit, input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent  implements OnInit {
+
+  @Input() title: string;
+  @Input() backButton: string;
+  @Input() isModal: boolean;
+  @Input() color: string;
+  @Input() centerTitle: boolean;
+
+  darkMode;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+
+  setTheme(darkMode: boolean){
+    this.darkMode = darkMode;
+  }
+}
